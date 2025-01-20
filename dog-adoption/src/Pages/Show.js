06e020b498c0bb14811b3b74ff7dc5d739851177
Rachel <rachel.sun.rondon@gmail.com>
2025-01-20 11:46:11 -0500

    feat: display dog breeds

--- dog-adoption/src/Pages/Show.js
+++ dog-adoption/src/Pages/Show.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from 'react';
+
 const Show = () => {
+
+  const api = "https://frontend-take-home-service.fetch.com";
+  const [dogBreeds, setDogBreeds] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(`${api}/dogs/breeds`, {
+        method: "GET", 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(),
+        credentials: "include"
+      });
+      const data = await response.json();
+      setDogBreeds(data);
+    }
+
+    getData()
+  }, []);
+
+  console.log(dogBreeds);
   return (
     <div>
       <p>List of Dogs</p>
+      {dogBreeds.map((breed) => {
+        return (
+          <h2>
+            {breed}
+          </h2>
+        )
+      })}
     </div>
   )
 };
