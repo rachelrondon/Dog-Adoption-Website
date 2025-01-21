f971036778f832dd98ce5c0535cb34245ae0eecd
Rachel <rachel.sun.rondon@gmail.com>
2025-01-21 10:51:30 -0500

    feat: filter dogs based on breed

--- dog-adoption/src/Pages/Show.js
+++ dog-adoption/src/Pages/Show.js
@@ -7,11 +7,31 @@ const Show = () => {
   const api = "https://frontend-take-home-service.fetch.com";
   const [dogIds, setDogIds] = useState("");
   const [dogs, setDogs] = useState("");
-  const sortBreedAscending = 'sort=breed:asc';
+  const [dogBreed, setDogBreed] = useState("");
+  const [sortBy, setSortBy] = useState('sort=breed:asc');
+
+  /*
+  useEffect(() => {
+    async function getDogBreeds() {
+      const response = await fetch(`${api}/dogs/breeds`, {
+        method: "GET", 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(),
+        credentials: "include"
+      }).catch((error) => console.log('error'));
+      const data = await response.json();
+      setDogBreed(data);
+    }
+    getDogBreeds();
+
+  }, []);*/ 
+
 
   useEffect(() => {
 
-    async function getData(sortBy) {
+    async function getData() {
       const response = await fetch(`${api}/dogs/search?${sortBy}`, {
         method: "GET", 
         headers: {
@@ -24,7 +44,7 @@ const Show = () => {
       setDogIds(data.resultIds);
     }
 
-    getData(sortBreedAscending)
+    getData()
   }, []);
   
     useEffect(() => {
@@ -50,9 +70,18 @@ const Show = () => {
 
     }, [dogIds])
 
+    const searchByDogBreed = (formData) => {
+      const dogBreed = formData.get("query");
+      alert(`Dog Breed: ${dogBreed}`);
+    };
 
   return (
     <div className="card-container">
+      <form className="dropdown">
+        <input name="query"  />
+        <button formAction={searchByDogBreed} /* type="submit"*/>Search</button>
+      </form>
+
       {dogs ? (
         <div className="card-layout">
         {[...dogs].map((dog) => {
